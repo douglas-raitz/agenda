d454e19332392558d8a26b95e4c9362d974f9755
Douglas Raitz <douglas_raitz@hotmail.com>
2022-12-22 13:52:44 -0300

    Corrigido erro de acesso ao usuario bloqueado através da url

--- projeto_agenda/contatos/views.py
+++ projeto_agenda/contatos/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('nome').filter(mostrar=True)
     paginator = Paginator(contatos, 20)
 
     page = request.GET.get('p')
@@ -16,6 +17,10 @@ def index(request):
 
 def ver_contato(request,contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
